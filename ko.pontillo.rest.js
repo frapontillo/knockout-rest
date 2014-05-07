136cca651181dd330be46f874a36aae8a3f72ea3
pwilczynskiclearcode <p.wilczynski@clearcode.cc>
2014-05-07 14:12:39 +0200

    Accidental `data` global removal.

--- ko.pontillo.rest.js
+++ ko.pontillo.rest.js
@@ -101,8 +101,7 @@ ko.pontillo.rest.entity = function (dataModel) {
 
     item.newEntity = function () {
         // Set the new data
-        data = newDataModel || {};
-        item.setData(data);
+        item.setData(newDataModel || {});
         item.isLoaded(false);
     };
 
